2a4dfe0e4fc61684ff374d760de8b5ab1612f53b
MukhriddinMike <miwady@mail.ru>
2019-02-04 04:19:08 +0900

    got motivated and started

---
+++ Level1/question2.py
@@ -0,0 +1,18 @@
+'''
+/Question:
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+/
+/
+'''
+
+for i in range(1,N+1):
+    m=m*i
+print(m)
+# I will be back and will make it easier
+# map,list comp, lambda etc.
+
